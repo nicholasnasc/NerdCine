8f742a82b0bb586de0d6319cb3515edd26f613f5
Nicholas <49096411+Nickz1n@users.noreply.github.com>
2024-02-19 18:11:03 -0300

    0.1

---
+++ app/config/db.config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Define database credentials
+ * 
+*/
+define("DB_HOST", "localhost"); 
+define("DB_NAME", "nerdcine");
+define("DB_USER", "root"); 
+define("DB_PASS", ""); 
+define("DB_ENCODING", "utf8");
